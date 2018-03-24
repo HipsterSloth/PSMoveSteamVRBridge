6fe3ecb580054eeb6db3ac60f9ead06d86462eb4
Stephen O'Hair <stephen.ohair@gmail.com>
2018-03-24 16:37:27 +1100

    refactor build

--- src/main/resources/scripts/msvc/GenerateProjectFiles.bat
+++ src/main/resources/scripts/msvc/GenerateProjectFiles.bat
@@ -1,13 +1,17 @@
 @echo off
 call SetBuildVars.bat
 
-set PROJECT_ROOT=..\..
+set PROJECT_ROOT=..\..\..\..\..
 
 IF NOT EXIST %PROJECT_ROOT%\build mkdir %PROJECT_ROOT%\build
 pushd %PROJECT_ROOT%\build
 
 echo "Rebuilding PSMoveSteamVRBridge Project files..."
-cmake .. -G %BUILD_CONFIGURATION% -DPSMOVESERVICE_BUILD_URL="%PSM_ZIP_FILE_PATH%"
+echo "Running cmake in %PROJECT_ROOT%
+cmake .. -G %MSBUILD_PARAMS%^
+ -DPSMOVESERVICE_PACKAGE_URL="%PSMOVESERVICE_BUILD_URL%"^
+ -DOPENVR_PACKAGE_URL="%OPENVR_PACKAGE_URL%"
+ -DPSMOVESTEAMVRBRIDGE_VERSION="%PSMOVESTEAMVRBRIDGE_VERSION%"^
 pause
 
 popd
--- src/main/resources/scripts/msvc/InitialSetupOfficialPSM_X64.bat
+++ src/main/resources/scripts/msvc/InitialSetupOfficialPSM_X64.bat
@@ -1,40 +1,84 @@
 @echo off
 setlocal
 
-set PROJECT_ROOT=..\..
+::Initialise
+set PROJECT_ROOT=..\..\..\..\..
+set BUILD_PROPS_FILE=%PROJECT_ROOT%\src\main\resources\configuration\build.properties
+
+::Clean
+call :cleanBuildFolder || goto handleError
+call :cleanDependencies || goto handleError
+
+::Load configuration properties
+call :loadBuildProperties || goto handleError
+
+::Set build variables
+call :setBuildVars || goto handleError
+
+::Generate the project files for PSMoveService
+REM call GenerateProjectFiles.bat || goto handleError
+
+::Exit batch script
+echo "MSVC solution generated successfully"
+goto exit
+
+
+::---------------------------
+::|Function definitions below
+::---------------------------
 
 ::Clean up the old PSMoveSteamVRBridge build folder
+:cleanBuildFolder
 IF EXIST %PROJECT_ROOT%\build (
 echo "Cleaning old build folder..."
 del /f /s /q %PROJECT_ROOT%\build > nul
 rmdir /s /q %PROJECT_ROOT%\build
 )
+goto:eof
 
 ::Clean up the old PSMoveSteamVRBridge deps folder
+:cleanDependencies
 IF EXIST %PROJECT_ROOT%\deps (
 echo "Cleaning old deps folder..."
 del /f /s /q %PROJECT_ROOT%\deps > nul
 rmdir /s /q %PROJECT_ROOT%\deps
 )
+goto:eof
 
-::Get the official PSMoveService build URL
-set /p PSM_OFFICIAL_BUILD_URL=<%PROJECT_ROOT%\PSMoveServiceReleaseURL.txt
-echo "Setting build URL to %PSM_OFFICIAL_BUILD_URL%"
 
-:: Write out build parameters config batch file
+::Function loads properties into the SetBuildVars batch file
+:loadBuildProperties
+echo Loading properties from %BUILD_PROPS_FILE%
+call :loadBuildProperty "psmoveservice.pacakge.url"  %BUILD_PROPS_FILE% PSM_PACKAGE_URL
+call :loadBuildProperty "openvr.pacakge.url" %BUILD_PROPS_FILE% OPENVR_PACAKGE_URL
+call :loadBuildProperty "psmovesteamvrbridge.version" %BUILD_PROPS_FILE% PSMOVESTEAMVRBRIDGE_VERSION
+call :loadBuildProperty "msbuild.parameters" %BUILD_PROPS_FILE% MSBUILD_PARAMS
+echo Properties loaded into SetBuildVars.bat
+goto:eof
+
+::Fuction returns a configured build property value for the given key
+:loadBuildProperty
+set PROP_KEY=%1
+set FILE=%2
+echo "Loading %PROP_KEY% from %FILE%"
+for /f "tokens=2,2 delims==" %%i in ('findstr /i %PROP_KEY% %FILE%') do set %3=%%i
+goto:eof
+
+::Function sets the build variables in a batch file called SetBuildVars.bat
+:setBuildVars
 del SetBuildVars.bat
 echo @echo off >> SetBuildVars.bat
-echo set BUILD_CONFIGURATION="Visual Studio 15 2017 Win64" >> SetBuildVars.bat
-echo set PSM_ZIP_FILE_PATH=%PSM_OFFICIAL_BUILD_URL% >> SetBuildVars.bat
+echo set PSM_PACKAGE_URL="%PSM_PACKAGE_URL%">> SetBuildVars.bat
+echo set OPENVR_PACAKGE_URL="%OPENVR_PACAKGE_URL%">> SetBuildVars.bat
+echo set PSMOVESTEAMVRBRIDGE_VERSION="%PSMOVESTEAMVRBRIDGE_VERSION%">> SetBuildVars.bat
+echo set MSBUILD_PARAMS="%MSBUILD_PARAMS%">> SetBuildVars.bat
+goto:eof
 
-:: Generate the project files for PSMoveService
-call GenerateProjectFiles.bat
+:handleError
 pause
-goto exit
-
-:failure
-pause
-goto exit
+endlocal
+exit /b 1
 
 :exit
 endlocal
+exit /b 0
